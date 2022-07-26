38bcc0797d2af9c735f039e285b6d090b07e0d96
Phelipe Sempreboni <phelipe.utsempreboni@outlook.com>
2022-07-25 22:50:16 -0300

    Criação e atualização de script em R.

---
+++ trainings/data science academy/big data analytics with r and microsoft azure machine learning/2 - fundamentals of r language/repository/5 - basic data type.R
@@ -0,0 +1,10 @@
+# Variáveis em R
+
+# Obs: Caso tenha problemas com a acentuação, consulte este link:
+# https://support.rstudio.com/hc/en-us/articles/20053197-character-Enconding
+
+# Configurando o diretório de trabalho
+# Coloque entre aspas o diretório de trabalho que você está usando no seu computador
+# Não use diretórios com espaço no nome
+setwd("C:/Users/pheli/OneDrive/Archives/2 - Github/Repositories/data-science/trainings/data science academy/big data analytics with r and microsoft azure machine learning/2 - fundamentals of r language/repository")
+getwd()
