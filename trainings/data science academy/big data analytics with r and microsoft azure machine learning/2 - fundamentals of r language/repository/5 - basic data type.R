a339733b73c256a6db7218b606cc2c0d652fcb43
Phelipe Sempreboni <phelipe.utsempreboni@outlook.com>
2022-07-26 08:24:50 -0300

    Atualização de script em R.

--- trainings/data science academy/big data analytics with r and microsoft azure machine learning/2 - fundamentals of r language/repository/5 - basic data type.R
+++ trainings/data science academy/big data analytics with r and microsoft azure machine learning/2 - fundamentals of r language/repository/5 - basic data type.R
@@ -8,3 +8,31 @@
 # Não use diretórios com espaço no nome
 setwd("C:/Users/pheli/OneDrive/Archives/2 - Github/Repositories/data-science/trainings/data science academy/big data analytics with r and microsoft azure machine learning/2 - fundamentals of r language/repository")
 getwd()
+
+# Numeric - Todos os números criados em R são do modo numeric
+# São armazenados como números decimais (double)
+num1 <- 7
+num1
+class(num1)
+mode(num1)
+typeof(num1)
+
+num2 = 16.82
+num2
+mode(num2)
+typeof(num2)
+
+# Integer
+# Convertemos tipos numeric para integer
+is.integer(num2)
+y = as.integer(num2)
+class(y)
+mode(y)
+typeof(y)
+
+as.integer('3.17')
+as.integer("Joe")
+as.integer('Joe')
+as.integer(TRUE)
+
+
