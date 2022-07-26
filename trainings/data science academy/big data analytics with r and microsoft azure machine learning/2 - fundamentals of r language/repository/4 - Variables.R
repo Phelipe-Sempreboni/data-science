4a41403deeb3f6bac7ac6f279d5955f7403880ae
Phelipe Sempreboni <phelipe.utsempreboni@outlook.com>
2022-07-25 22:36:18 -0300

    Atualização de script.

--- trainings/data science academy/big data analytics with r and microsoft azure machine learning/2 - fundamentals of r language/repository/4 - Variables.R
+++ trainings/data science academy/big data analytics with r and microsoft azure machine learning/2 - fundamentals of r language/repository/4 - Variables.R
@@ -1,4 +1,4 @@
-# Operadores Básicos, Relacionais e Lógicos em R
+# Variáveis em R
 
 # Obs: Caso tenha problemas com a acentuação, consulte este link:
 # https://support.rstudio.com/hc/en-us/articles/20053197-character-Enconding
@@ -8,3 +8,34 @@
 # Não use diretórios com espaço no nome
 setwd("C:/Users/pheli/OneDrive/Archives/2 - Github/Repositories/data-science/trainings/data science academy/big data analytics with r and microsoft azure machine learning/2 - fundamentals of r language/repository")
 getwd()
+
+# Criando variáveis
+var1 = 100
+var1
+mode(var1)
+typeof(var1)
+help("mode")
+sqrt(var1)
+
+# Podemos atribuir o valor de uma variável a outr variável
+var2 = var1
+var2
+mode(var2)
+typeof(var2)
+help("typeof")
+
+# Uma variável pode ser uma lista de elementos, igual uma lista (list()) em Python
+var3 = c("primeiro", "segundo", "terceiro")
+var3
+mode(var3)
+typeof(var3)
+
+# Uma variável pode ser uma função
+var4 = function(x) {x+3}
+var4
+mode(var4)
+
+
+
+
+
